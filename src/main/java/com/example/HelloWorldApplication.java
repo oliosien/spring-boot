6f94fe1b52bb890b4b4ed583f42551a6f4fffb40
Enrico Oliosi <enrico.oliosi@intesys.it>
2025-11-05 17:53:50 +0100

    Initial commit

---
+++ src/main/java/com/example/HelloWorldApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+
+}
